f7c2b50df427788dca3464702066904be348be7f
ahmad4433 <aliraza126005@gmail.com>
2024-08-15 18:48:49 +0100

    initial

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import reactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+// import './assets/scss/app.scss'; // Import your main SASS file
+
+
+reactDOM.createRoot(document.getElementById('root')).render(<App />)
